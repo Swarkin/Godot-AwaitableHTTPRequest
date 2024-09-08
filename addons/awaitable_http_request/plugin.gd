534ea392ad6f5c9ee6443089476a52b8802ef6da
nanachi <102416174+Swarkin@users.noreply.github.com>
2024-09-08 10:31:21 +0200

    Add HTTPResult as custom type
    Not sure if this is necessary, but it seems to be good practice?

--- addons/awaitable_http_request/plugin.gd
+++ addons/awaitable_http_request/plugin.gd
@@ -4,6 +4,8 @@ extends EditorPlugin
 
 func _enter_tree():
 	add_custom_type("AwaitableHTTPRequest", "HTTPRequest", preload("awaitable_http_request.gd"), preload("icon.png"))
+	add_custom_type("HTTPResult", "RefCounted", preload("http_result.gd"), preload("icon.png"))
 
 func _exit_tree():
 	remove_custom_type("AwaitableHTTPRequest")
+	remove_custom_type("HTTPResult")
